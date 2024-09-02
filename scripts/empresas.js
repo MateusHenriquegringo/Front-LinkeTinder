11ff53eae355310bf4bb3c6ec900b4773630b78f
ubuntu_mateus <henriquemateus2502@gmail.com>
2024-09-02 15:48:47 -0300

    compiled ts

--- scripts/empresas.js
+++ scripts/empresas.js
@@ -2,11 +2,35 @@
 const button = document.getElementById("anunciarVaga");
 const modal = document.getElementById("vagaModal");
 const closeButton = document.getElementsByClassName("close")[0];
+const form = document.getElementById('vagaForm');
 button === null || button === void 0 ? void 0 : button.addEventListener('click', function (e) {
     e.preventDefault();
     openModal();
 });
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nomeVaga = document.getElementById('nomeVaga');
+    const descricao = document.getElementById('descricao');
+    const empresa = document.getElementById('empresa');
+    const email = document.getElementById('email');
+    const cep = document.getElementById('cep');
+    const cidade = document.getElementById('cidade');
+    const estado = document.getElementById('estadoFederativo');
+    const vagaData = {
+        nome: nomeVaga.value,
+        descricao: descricao.value,
+        empresa: empresa.value,
+        email: email.value,
+        cep: cep.value,
+        cidade: cidade.value,
+        estadoFederativo: estado.value
+    };
+    const uniqueKey = `vagaData_${new Date().getTime()}`;
+    localStorage.setItem(uniqueKey, JSON.stringify(vagaData));
+    alert("dados salvos com sucesso");
+});
 modal === null || modal === void 0 ? void 0 : modal.addEventListener('click', (e) => {
+    e.preventDefault();
     if (e.target == modal || e.target == closeButton) {
         closeModal();
     }
--- typescript/empresas.ts
+++ typescript/empresas.ts
@@ -11,18 +11,53 @@ button?.addEventListener('click', function (e) {
 
 })
 
-form?.addEventListener('submit', (e)=> {
-    e.preventDefault
+interface VagaDeEmprego {
+    nome: string,
+    descricao: string,
+    empresa: string
+    email: string
+    cep: string
+    cidade: string
+    estadoFederativo: string
+}
+
+form?.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const nomeVaga = document.getElementById('nomeVaga') as HTMLInputElement
+    const descricao = document.getElementById('descricao') as HTMLInputElement
+    const empresa = document.getElementById('empresa') as HTMLInputElement
+    const email = document.getElementById('email') as HTMLInputElement
+    const cep = document.getElementById('cep') as HTMLInputElement
+    const cidade = document.getElementById('cidade') as HTMLInputElement
+    const estado = document.getElementById('estadoFederativo') as HTMLInputElement
+
+    const vagaData: VagaDeEmprego = {
+        nome: nomeVaga.value,
+        descricao: descricao.value,
+        empresa: empresa.value,
+        email: email.value,
+        cep: cep.value,
+        cidade: cidade.value,
+        estadoFederativo: estado.value
+    };
+
+    const uniqueKey: string = `vagaData_${new Date().getTime()}`;
+
+    localStorage.setItem(uniqueKey, JSON.stringify(vagaData))
+    
+    alert("dados salvos com sucesso")
+
 })
 
 
-modal?.addEventListener('click', (e)=>{
-   e.preventDefault()
+modal?.addEventListener('click', (e) => {
+    e.preventDefault()
 
-    if(e.target == modal || e.target == closeButton){
+    if (e.target == modal || e.target == closeButton) {
         closeModal()
     }
- }
+}
 )
 
 
