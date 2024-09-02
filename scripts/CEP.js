752eec72410d476bd5c47f211dbb799bf75e56bf
ubuntu_mateus <henriquemateus2502@gmail.com>
2024-09-02 15:47:52 -0300

    add viaCEP api to autocomplete inputs

---
+++ scripts/CEP.js
@@ -0,0 +1,41 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const cepInput = document.getElementById("cep");
+cepInput === null || cepInput === void 0 ? void 0 : cepInput.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    let cep = cepInput.value;
+    if (validateCEP(cep)) {
+        getData(cep, autocompleteInputs);
+    }
+});
+function validateCEP(cep) {
+    cep = cep.replace(/\D/g, '');
+    const REGEX = /^[0-9]{8}$/;
+    return REGEX.test(cep);
+}
+function getData(cep, callback) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(`https://viacep.com.br/ws/${cep}/json/`);
+            const data = yield response.json();
+            callback(data);
+        }
+        catch (e) {
+            console.log(e);
+        }
+    });
+}
+function autocompleteInputs(data) {
+    const city = document.getElementById("cidade");
+    const state = document.getElementById("estadoFederativo");
+    city.value = data.localidade;
+    state.value = data.uf;
+}
---
+++ typescript/CEP.ts
@@ -0,0 +1,53 @@
+const cepInput = document.getElementById("cep") as HTMLInputElement
+
+type responseCep = {
+    localidade: string,
+    uf: string
+}
+
+cepInput?.addEventListener('keyup', (e) => {
+    e.preventDefault()
+
+    let cep: string = cepInput.value
+
+    if (validateCEP(cep)) { getData(cep, autocompleteInputs) }
+    
+    }
+)
+
+
+function validateCEP(cep: string): boolean {
+
+    cep = cep.replace(/\D/g, '');
+
+    const REGEX = /^[0-9]{8}$/;
+
+    return REGEX.test(cep);
+}
+
+async function getData(cep: string, callback: Function) : Promise<void> {
+
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`
+        )
+
+        const data: responseCep = await response.json()
+
+        callback(data)
+
+    } catch (e) { console.log(e) }
+
+    
+}
+
+
+function autocompleteInputs(data: responseCep): void {
+    
+    const city = document.getElementById("cidade") as HTMLInputElement
+    const state = document.getElementById("estadoFederativo") as HTMLInputElement
+
+    city.value = data.localidade
+    state.value = data.uf
+}
+
+
